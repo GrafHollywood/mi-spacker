746890bd2d8b38932c4e1ede26817c00eae9c378
Матвей Калинин <m_kalinin@dev-city.local>
2022-10-31 16:21:42 +0300

    create basic terminal window

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import {Terminal} from '../components/terminal';
 import styles from './App.module.scss';
 
 export function App() {
-	return <div className={styles['app']}>Hello world!</div>;
+	return (
+		<div className={styles['app']}>
+			<Terminal />
+		</div>
+	);
 }
---
+++ src/components/terminal/Terminal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './Terminal.module.scss';
+
+export const Terminal = () => {
+	return <div className={styles['terminal']}>Hello world!</div>;
+};
