8314722c3b9d928aa6c2081646fe7d358c74446e
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-11-30 12:07:35 +0300

    add chakra-ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,15 @@
+import {Button, useColorMode} from '@chakra-ui/react';
 import React from 'react';
 
 import styles from './App.module.scss';
 
 export function App() {
-	return <div className={styles['app']}>Hello world!</div>;
+	const {colorMode, toggleColorMode} = useColorMode();
+	return (
+		<div className={styles['app']}>
+			<Button onClick={toggleColorMode}>
+				Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+			</Button>
+		</div>
+	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import './index.scss';
 
+import {ChakraProvider, ColorModeScript} from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {App} from './app';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<ChakraProvider>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<App />
+		</ChakraProvider>
 	</React.StrictMode>
 );
 
